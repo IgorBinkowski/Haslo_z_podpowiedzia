edc8e8819b363b599cc37a41dd36df8b07ce15d3
budzik12 <81246476+budzik12@users.noreply.github.com>
2023-01-30 15:46:22 +0100

    Text commit

--- app/src/main/java/com/nkodem/haslozpodpowiedzi/MainActivity.kt
+++ app/src/main/java/com/nkodem/haslozpodpowiedzi/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.nkodem.haslozpodpowiedzi
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,11 +16,13 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.Sprawdź).setOnClickListener {
             var password = findViewById<TextInputEditText>(R.id.Hasło).text.toString()
             var potwierdź = findViewById<TextInputEditText>(R.id.Potwierdź).text.toString()
-            if (password == potwierdź){
-                findViewById<TextView>(R.id.textView).text = "Hasła: Zgodne"}
-            else {
-                findViewById<TextView>(R.id.textView).text = " Hasła: Niezgodne"}
+            val CheckedTextView = findViewById<TextView>(R.id.Zgodne)
+
+            if (password == "haslo"){
+                android.R.drawable.checkbox_on_background
+            }
+                else{
+                android.R.drawable.checkbox_off_background}
         }
-    }
     }
 }
